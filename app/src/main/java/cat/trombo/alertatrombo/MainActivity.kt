e06c830fc1963b1de6f3022cff594c0af075199c
agusti <agustigm@gmail.com>
2022-12-16 22:40:12 +0100

    initial commit

---
+++ app/src/main/java/cat/trombo/alertatrombo/MainActivity.kt
@@ -0,0 +1,40 @@
+package cat.trombo.alertatrombo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import cat.trombo.alertatrombo.ui.theme.AlertaTromboTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AlertaTromboTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AlertaTromboTheme {
+        Greeting("Android")
+    }
+}
