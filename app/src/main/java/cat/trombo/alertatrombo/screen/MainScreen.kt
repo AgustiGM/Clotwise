1b32e7d316aa8b9962b186dffb254784c3f0103e
Superjuane <juan.emilio.olivan@estudiantat.upc.edu>
2022-12-17 19:23:13 +0100

    barra i popup(progress)

--- app/src/main/java/cat/trombo/alertatrombo/screen/MainScreen.kt
+++ app/src/main/java/cat/trombo/alertatrombo/screen/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
@@ -16,6 +17,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import cat.trombo.alertatrombo.R
@@ -30,6 +32,11 @@ fun MainScreen(navController: NavHostController) {
     val shape = RoundedCornerShape(12.dp)
     val viewModel = MainScreenVM(LocalContext.current)
     val p: Person = viewModel.getPerson(LocalContext.current)
+    var pop by remember {mutableStateOf(false)}
+    var o1 = ""
+    var o2 = ""
+
+
     println(p.height)
     Box {
         Image(
@@ -49,16 +56,20 @@ fun MainScreen(navController: NavHostController) {
                     .padding(8.dp)
                     .fillMaxWidth()
                     .clip(shape)
-                    .background(color = Color.Red.copy(alpha = 0.5f))
-                    .height(100.dp)/*, backgroundColor = Color.Red*/
+                    .background(color = LightBackground2.copy(alpha = 0.75f))
+                    .height(200.dp)
+                    /*, backgroundColor = Color.Red*/
             ) {
-                Text("Top box"/*, style = TextStyle(color = Color.White, textAlign = TextAlign.Center)*/)
+                CustomProgressBar()
             }
+
+            Button(onClick = {pop = true;}){Text("click")}
 
             Box(
                 modifier = Modifier
                     .clip(RoundedCornerShape(20.dp))
                     .padding(8.dp)
+                    .fillMaxWidth()
                     .background(color = Color.Green)
                     .height(100.dp)
             )
@@ -66,10 +77,32 @@ fun MainScreen(navController: NavHostController) {
             Box(modifier = Modifier
                 .fillMaxWidth()
                 .height(250.dp)
-                .background(color = Background )) {
+                .background(color = LightBackground)) {
             //TabOnlyTitle()
             //CustomTabs()
             tabs(p)
+            }
+        }
+        if(pop){
+            Popup(
+                alignment = Alignment.CenterStart,
+
+                ) {
+                Box(modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+                    .clip(shape)
+                    .background(color = Color.White)
+                    .height(200.dp)){
+                    Row() {
+                        Button(onClick = { o1 = "1"; pop = false }) {
+                            Text("option 1")
+                        }
+                        Button(onClick = { o2 = "2"; pop = false }) {
+                            Text("option 2")
+                        }
+                    }
+                }
             }
         }
     }
@@ -85,7 +118,7 @@ fun tabs(p:Person) {
             tabTitles.forEachIndexed { index, title ->
                 Tab(selected = tabIndex == index, // 4.
                     onClick = { tabIndex = index },
-                     modifier = Modifier.background(color = DarkBackground),
+                     modifier = Modifier.background(color = LightBackground2),
                     text = { Text(text = title) }) // 5.
             }
         }
@@ -120,7 +153,8 @@ fun tabs(p:Person) {
                 .padding(6.dp)
                 .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround){
                 Column(modifier = Modifier
-                    .padding(15.dp).fillMaxHeight(), verticalArrangement = Arrangement.SpaceEvenly){
+                    .padding(15.dp)
+                    .fillMaxHeight(), verticalArrangement = Arrangement.SpaceEvenly){
                     Text("Nom: "+p.name)
                     Text("Edat: "+p.age)
                     Text("Gènere: "+p.gender)
--- app/src/main/java/cat/trombo/alertatrombo/viewmodels/MainScreenVM.kt
+++ app/src/main/java/cat/trombo/alertatrombo/viewmodels/MainScreenVM.kt
@@ -1,6 +1,8 @@
 package cat.trombo.alertatrombo.viewmodels
 
 import android.content.Context
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import cat.trombo.alertatrombo.domain.*
 import cat.trombo.alertatrombo.repo.JsonObjectRepo
@@ -16,7 +18,6 @@ class MainScreenVM (context: Context) {
 //    val currentUser : Person? = null
 
     val repo = JsonPersonDataRepo();
-
     fun getPerson(context: Context):Person{
        var p:Person? = repo.loadData(context, "agusti.json" );
         println(p?.name)
