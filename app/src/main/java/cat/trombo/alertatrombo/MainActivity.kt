bd3d19807b20ee1a2815d4d9405f01570ddad120
Superjuane <juan.emilio.olivan@estudiantat.upc.edu>
2022-12-17 12:39:02 +0100

    first commit on main screen

--- app/src/main/java/cat/trombo/alertatrombo/MainActivity.kt
+++ app/src/main/java/cat/trombo/alertatrombo/MainActivity.kt
@@ -3,14 +3,12 @@ package cat.trombo.alertatrombo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import cat.trombo.alertatrombo.ui.theme.AlertaTromboTheme
+import androidx.compose.material.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +17,8 @@ class MainActivity : ComponentActivity() {
             AlertaTromboTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    //Greeting("Android")
+                    MainScreen()
                 }
             }
         }
