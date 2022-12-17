19324ca91238ea282a4dc0b4016002b3404993e8
Superjuane <juan.emilio.olivan@estudiantat.upc.edu>
2022-12-17 15:25:45 +0100

    Avances

--- app/src/main/java/cat/trombo/alertatrombo/MainActivity.kt
+++ app/src/main/java/cat/trombo/alertatrombo/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
             AlertaTromboTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    ScreenMain()
+                    MainScreen()
                 }
             }
         }
